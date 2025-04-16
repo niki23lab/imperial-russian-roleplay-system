4c5223230d63c6dd64fdb49d4d28b2745e9bff29
niki23lab <imperialcourtrussia@gmail.com>
2025-04-16 12:05:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ status: "ok", message: "Registered successfully" }));
       } catch (err) {
-        console.error("❌ Error:", err.message);
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Error en el servidor" }));
-      }
+  console.error("❌ Error en el registro:", err);
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ error: "Error en el servidor", detalle: err.message }));
+}
     });
   } else {
     res.writeHead(404);
